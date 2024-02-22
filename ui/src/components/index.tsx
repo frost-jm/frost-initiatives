06f4c0294d15a0250b8a68465c05bc46a56ed90e
Marie <marie.garbin@frostdesigngroup.com>
2024-02-22 14:31:27 +0800

    added header logo

--- ui/src/components/index.tsx
+++ ui/src/components/index.tsx
@@ -1,5 +1,6 @@
 // Import all the components and export at once.
 
 import Sample from './sample';
+import logo from './low-level-components/logo';
 
-export { Sample };
+export { Sample, logo };
--- ui/src/pages/components-library.tsx
+++ ui/src/pages/components-library.tsx
@@ -1,44 +1,40 @@
 import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
-import { Sample } from '@/components/index';
+import { Sample, logo } from '@/components/index';
 
 // Add the component here
-const components = [{ name: 'Sample Component', component: Sample }];
+const components = [
+  { name: 'Sample Component', component: Sample },
+  { name: 'Page logo', component: logo },
+];
 
 const ComponentsLibrary = () => {
-	return (
-		<Box padding={4}>
-			<Typography
-				variant='h2'
-				gutterBottom
-			>
-				Component Library
-			</Typography>
-			<Grid
-				container
-				spacing={3}
-			>
-				{components.map(({ name, component: Component }) => (
-					<Grid
-						item
-						xs={12}
-						key={name}
-					>
-						<Card style={{ border: '1px solid #000', boxShadow: 'none' }}>
-							<CardContent>
-								<Typography
-									variant='h6'
-									gutterBottom
-								>
-									{name}
-								</Typography>
-								<Component />
-							</CardContent>
-						</Card>
-					</Grid>
-				))}
-			</Grid>
-		</Box>
-	);
+  return (
+    <Box padding={4}>
+      <Typography variant="h2" gutterBottom>
+        Component Library
+      </Typography>
+      <Grid container spacing={3}>
+        {components.map(({ name, component: Component }) => (
+          <Grid item xs={12} key={name}>
+            <Card
+              style={{
+                border: '1px solid #000',
+                boxShadow: 'none',
+                background: '#B4B4B4',
+              }}
+            >
+              <CardContent>
+                <Typography variant="h6" gutterBottom>
+                  {name}
+                </Typography>
+                <Component />
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default ComponentsLibrary;
