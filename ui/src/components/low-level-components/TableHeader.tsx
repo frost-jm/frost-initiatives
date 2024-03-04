23ff996e36d7d3489b8bb37bcfc8db60639e255e
Marie-Frost <149762948+Marie-Frost@users.noreply.github.com>
2024-03-04 16:11:50 +0800

    Component/table-header (#11)
    * Added table header
    
    * Added props

---
+++ ui/src/components/low-level-components/TableHeader.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@mui/material';
+
+const TableHeader = ({ text, quantity }) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+      <Box
+        sx={{
+          fontFamily: 'Figtree-Bold',
+          fontWeight: '700',
+          fontSize: '20px',
+          lineHeight: '1',
+          color: 'var(--input-color)',
+        }}
+      >
+        {text}
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: '#E7EEF0',
+          padding: '4px 8px',
+          borderRadius: '5px',
+          fontFamily: 'Figtree-Bold',
+          fontWeight: '700',
+          fontSize: '14px',
+          lineHeight: '1',
+          color: 'rgba(29, 36, 79, 0.6)',
+        }}
+      >
+        {quantity}
+      </Box>
+    </Box>
+  );
+};
+export default TableHeader;
