835dbaa0a7d61466527ea900827c013e4d95f942
Marie <marie.garbin@frostdesigngroup.com>
2024-03-04 14:19:04 +0800

    Added table header

--- ui/src/components/index.tsx
+++ ui/src/components/index.tsx
@@ -10,6 +10,19 @@ import EditorTabs from '@/components/low-level-components/EditorTabs';
 import DepartmentDropdown from '@/components/low-level-components/Dropdown/DepartmentDropdown';
 import BannerContent from '@/components/low-level-components/BannerContent';
 import StatusHeader from '@/components/low-level-components/Header/StatusHeader';
+import TableHeader from './low-level-components/TableHeader';
 import Sample from '@/components/sample';
 
-export { Sample, Logo, Input, Button, StatusDropdown, ActionDropdown, EditorTabs, DepartmentDropdown, BannerContent, StatusHeader };
+export {
+  Sample,
+  Logo,
+  Input,
+  Button,
+  StatusDropdown,
+  ActionDropdown,
+  EditorTabs,
+  DepartmentDropdown,
+  BannerContent,
+  StatusHeader,
+  TableHeader,
+};
--- ui/src/pages/components-library.tsx
+++ ui/src/pages/components-library.tsx
@@ -1,80 +1,84 @@
 import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
-import { Sample, Logo, Button, Input, StatusDropdown, ActionDropdown, EditorTabs, DepartmentDropdown, BannerContent, StatusHeader } from '@/components/index';
+import {
+  Sample,
+  Logo,
+  Button,
+  Input,
+  StatusDropdown,
+  ActionDropdown,
+  EditorTabs,
+  DepartmentDropdown,
+  BannerContent,
+  StatusHeader,
+  TableHeader,
+} from '@/components/index';
 
 const tabLabelsData = [
-	{ label: 'Home', count: 0 },
-	{ label: 'For Voting', count: 5 },
-	{ label: 'For Implementation', count: 1 },
+  { label: 'Home', count: 0 },
+  { label: 'For Voting', count: 5 },
+  { label: 'For Implementation', count: 1 },
 ];
 
 // Add the components here
 const components = [
-	{ name: 'Sample Component', component: Sample },
-	{ name: 'Page logo', component: Logo },
-	{ name: 'Submit button', component: Button },
-	{ name: 'Status Dropdown', component: StatusDropdown },
-	{ name: 'Input Field', component: Input },
-	{ name: 'Comment Field', component: Input, props: { variant: 'comment' } },
-	{ name: 'Action Dropdown', component: ActionDropdown },
-	{ name: 'Department Dropdown', component: DepartmentDropdown },
-	{
-		name: 'Editor Tabs',
-		component: EditorTabs,
-		props: { tabLabels: tabLabelsData },
-	},
-	{
-		name: 'Banner Content',
-		component: BannerContent,
-		props: {
-			title: 'Got any suggestions for us?',
-			description: 'Let us know by pitching your own internal initiative.',
-		},
-	},
-	// To check different status: Change the status to any of the ff: 'Done', 'In Progress', 'For Implementation', 'Archived'
-	{ name: 'Status Header', component: StatusHeader, props: { status: 'Pending' } },
+  { name: 'Sample Component', component: Sample },
+  { name: 'Page logo', component: Logo },
+  { name: 'Submit button', component: Button },
+  { name: 'Status Dropdown', component: StatusDropdown },
+  { name: 'Input Field', component: Input },
+  { name: 'Comment Field', component: Input, props: { variant: 'comment' } },
+  { name: 'Action Dropdown', component: ActionDropdown },
+  { name: 'Department Dropdown', component: DepartmentDropdown },
+  { name: 'Table Header', component: TableHeader },
+  {
+    name: 'Editor Tabs',
+    component: EditorTabs,
+    props: { tabLabels: tabLabelsData },
+  },
+  {
+    name: 'Banner Content',
+    component: BannerContent,
+    props: {
+      title: 'Got any suggestions for us?',
+      description: 'Let us know by pitching your own internal initiative.',
+    },
+  },
+  // To check different status: Change the status to any of the ff: 'Done', 'In Progress', 'For Implementation', 'Archived'
+  {
+    name: 'Status Header',
+    component: StatusHeader,
+    props: { status: 'Pending' },
+  },
 ];
 
 const ComponentsLibrary = () => {
-	return (
-		<Box padding={4}>
-			<Typography
-				variant='h2'
-				gutterBottom
-			>
-				Component Library
-			</Typography>
-			<Grid
-				container
-				spacing={3}
-			>
-				{components.map(({ name, component: Component, props }) => (
-					<Grid
-						item
-						xs={12}
-						key={name}
-					>
-						<Card
-							style={{
-								border: '1px solid #000',
-								boxShadow: 'none',
-								background: '#B4B4B4',
-							}}
-						>
-							<CardContent>
-								<Typography
-									variant='h6'
-									gutterBottom
-								>
-									{name}
-								</Typography>
-								{props ? <Component {...props} /> : <Component />}
-							</CardContent>
-						</Card>
-					</Grid>
-				))}
-			</Grid>
-		</Box>
-	);
+  return (
+    <Box padding={4}>
+      <Typography variant='h2' gutterBottom>
+        Component Library
+      </Typography>
+      <Grid container spacing={3}>
+        {components.map(({ name, component: Component, props }) => (
+          <Grid item xs={12} key={name}>
+            <Card
+              style={{
+                border: '1px solid #000',
+                boxShadow: 'none',
+                background: '#B4B4B4',
+              }}
+            >
+              <CardContent>
+                <Typography variant='h6' gutterBottom>
+                  {name}
+                </Typography>
+                {props ? <Component {...props} /> : <Component />}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default ComponentsLibrary;
