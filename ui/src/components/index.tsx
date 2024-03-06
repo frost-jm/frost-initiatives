1f3a6df1e8914d5ad3a799521d7e003be6e5d412
Marie-Frost <149762948+Marie-Frost@users.noreply.github.com>
2024-03-06 13:15:42 +0800

    added table dropdowns (#22)

--- ui/src/components/index.tsx
+++ ui/src/components/index.tsx
@@ -12,12 +12,38 @@ import BannerContent from '@/components/low-level-components/BannerContent';
 import StatusHeader from '@/components/low-level-components/Header/StatusHeader';
 import TableHeader from '@/components/low-level-components/Table/TableHeader';
 import Avatar from '@/components/low-level-components/Avatar/Avatar';
-import Buttons, { ButtonType } from '@/components/low-level-components/Button/Buttons';
+import Buttons, {
+  ButtonType,
+} from '@/components/low-level-components/Button/Buttons';
 import TableLabel from '@/components/low-level-components/Table/TableLabel';
 import TableContent from './low-level-components/Table/TableContent';
 import PendingVote from './low-level-components/Header/PendingVote';
 import TableContentWTitle from './low-level-components/Table/TableContentWTitle';
 import ExpandButton from '@/components/low-level-components/Button/ExpandButton';
+import TableDropdown from './low-level-components/Dropdown/TableDropdown';
+import SortDropdown from '@/components/low-level-components/Dropdown/SortDropdown';
 import Sample from '@/components/sample';
 
-export { Sample, Logo, Input, SubmitButton, StatusDropdown, ActionDropdown, EditorTabs, DepartmentDropdown, BannerContent, StatusHeader, Buttons, ButtonType, TableHeader, Avatar, TableLabel, TableContent, PendingVote, TableContentWTitle, ExpandButton };
+export {
+  Sample,
+  Logo,
+  Input,
+  SubmitButton,
+  StatusDropdown,
+  ActionDropdown,
+  EditorTabs,
+  DepartmentDropdown,
+  BannerContent,
+  StatusHeader,
+  Buttons,
+  ButtonType,
+  TableHeader,
+  Avatar,
+  TableLabel,
+  TableContent,
+  PendingVote,
+  TableContentWTitle,
+  ExpandButton,
+  TableDropdown,
+  SortDropdown,
+};
