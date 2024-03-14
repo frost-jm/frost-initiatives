ad61409c4d3f1b03166f7137bb63e2c1a79273bf
Marie <marie.garbin@frostdesigngroup.com>
2024-03-14 15:06:12 +0800

    Added table section

--- ui/src/components/low-level-components/Card/InitiativeCard.tsx
+++ ui/src/components/low-level-components/Card/InitiativeCard.tsx
@@ -4,98 +4,92 @@ import ProgressBar from '../ProgressBar';
 import Buttons, { ButtonType } from '../Button/Buttons';
 
 interface InitiativeData {
-	title: string;
-	description: string;
-	pitcher: {
-		firstName: string;
-		lastName: string;
-	};
-	vote: number;
-	action: () => void;
+  title: string;
+  description: string;
+  pitcher: {
+    firstName: string;
+    lastName: string;
+  };
+  vote: number;
+  totalHeads: number;
+  action: () => void;
 }
 
 interface InitiativeCardProp {
-	data: InitiativeData;
+  data: InitiativeData;
 }
 const InitiativeCard = ({ data }: InitiativeCardProp) => {
-	const { title, description, pitcher, vote, action } = data;
-	return (
-		<Box
-			sx={{
-				width: '100%',
-				maxWidth: '341px',
-				borderRadius: '8px',
-				padding: '24px',
-				border: '1px solid #E8EEEE',
-				background: '#ffffff',
-				boxSizing: 'border-box',
-			}}
-		>
-			<Box>
-				<Box
-					sx={{
-						fontFamily: 'Figtree-Bold,sans-serif',
-						fontSize: '18px',
-						lineHeight: '1',
-						color: 'var(--input-color)',
-					}}
-				>
-					{title}
-				</Box>
-				<Box
-					sx={{
-						color: 'rgba(29, 36, 79, 0.8)',
-						fontFamily: 'Figtree-Regular,sans-serif',
-						fontSize: '14px',
-						lineHeight: '1.5',
-						marginTop: '8px',
-					}}
-				>
-					{description}
-				</Box>
-				<Box
-					sx={{
-						marginTop: '12px',
-					}}
-				>
-					<Box
-						sx={{
-							color: 'rgba(29, 36, 79, 0.4)',
-							fontFamily: 'Figtree-Regular,sans-serif',
-							fontSize: '12px',
-							lineHeight: '1.5',
-						}}
-					>
-						Pitched by:
-					</Box>
-					<Box marginTop='8px'>
-						<Avatar
-							label={true}
-							type='single'
-							data={pitcher}
-						/>
-					</Box>
-					<Box margin='24px 0'>
-						<ProgressBar
-							count={vote}
-							totalHeads={26}
-						/>
-					</Box>
-					<Box>
-						<Buttons
-							type={ButtonType.View}
-							maxWidth='100%'
-							background='#F2F5F7'
-							fontSize='14px'
-							action={action}
-						>
-							View details
-						</Buttons>
-					</Box>
-				</Box>
-			</Box>
-		</Box>
-	);
+  const { title, description, pitcher, vote, action } = data;
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: '341px',
+        borderRadius: '8px',
+        padding: '24px',
+        border: '1px solid #E8EEEE',
+        background: '#ffffff',
+        boxSizing: 'border-box',
+      }}
+    >
+      <Box>
+        <Box
+          sx={{
+            fontFamily: 'Figtree-Bold,sans-serif',
+            fontSize: '18px',
+            lineHeight: '1',
+            color: 'var(--input-color)',
+          }}
+        >
+          {title}
+        </Box>
+        <Box
+          sx={{
+            color: 'rgba(29, 36, 79, 0.8)',
+            fontFamily: 'Figtree-Regular,sans-serif',
+            fontSize: '14px',
+            lineHeight: '1.5',
+            marginTop: '8px',
+          }}
+        >
+          {description}
+        </Box>
+        <Box
+          sx={{
+            marginTop: '12px',
+          }}
+        >
+          <Box
+            sx={{
+              color: 'rgba(29, 36, 79, 0.4)',
+              fontFamily: 'Figtree-Regular,sans-serif',
+              fontSize: '12px',
+              lineHeight: '1.5',
+            }}
+          >
+            Pitched by:
+          </Box>
+          <Box marginTop='8px'>
+            <Avatar label={true} type='single' data={pitcher} />
+          </Box>
+          <Box margin='24px 0'>
+            <ProgressBar count={vote} totalHeads={26} />
+          </Box>
+          <Box>
+            <Buttons
+              type={ButtonType.View}
+              maxWidth='100%'
+              background='#F2F5F7'
+              fontSize='14px'
+              action={action}
+            >
+              View details
+            </Buttons>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default InitiativeCard;
