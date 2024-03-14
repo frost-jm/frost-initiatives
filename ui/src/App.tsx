06ca6eb7995a0212ff68424553b096229d0514a9
lxpocampo <enzo.ocampo@frostdesigngroup.com>
2024-03-14 11:43:12 +0800

    Removed unused variables and imports

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,9 @@
 import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
 
 import LogoHeader from './sections/LogoHeader';
 import TabsLanding from './sections/TabsLanding';
 
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from './context/userContext';
 
 const App = () => {
--- ui/src/context/userContext.tsx
+++ ui/src/context/userContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 interface UserContextData { 
