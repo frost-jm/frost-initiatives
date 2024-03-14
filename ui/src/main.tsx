505fe45f655f8fa515c6d9300717dbcc4088e6ef
lxpocampo <enzo.ocampo@frostdesigngroup.com>
2024-03-14 16:07:49 +0800

    Updated main.tsx
    Moved apollo and auth provider to authTokenContext to fix error in client when logging in

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import ComponentsLibrary from './pages/components-library.tsx';
 import Error from './pages/error-page.tsx';
-import { ApolloProvider } from '@apollo/client';
-import { NewApolloClient } from './graphql/apolloClient.tsx';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import { UserProvider } from './context/userContext.tsx';
-
+import { AuthTokenProvider } from './context/authTokenContext.tsx';
 
 const Index = () => {
-	const [token, setToken] = useState<string | undefined>('');
-
 	const router = createBrowserRouter([
 		{
 			path: '/',
@@ -27,23 +22,13 @@ const Index = () => {
 		},
 	]);
 	
-	const client = NewApolloClient(token ? token : undefined);
-
 	return (
 	  <React.StrictMode>
-		<Auth0Provider
-			domain={import.meta.env.VITE_AUTH0_DOMAIN}
-			clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-			authorizationParams={{
-				redirect_uri: window.location.origin,
-			}}
-		>
-			<ApolloProvider client={client}>
-				<UserProvider>
-					<RouterProvider router={router} /> 
-				</UserProvider>
-			</ApolloProvider>
-		</Auth0Provider>
+		<AuthTokenProvider>
+			<UserProvider>
+				<RouterProvider router={router} /> 
+			</UserProvider>
+		</AuthTokenProvider>
 	  </React.StrictMode>
 	);
 };
