04051ce47e659719868061cd94e5c1357a20fe5b
Marie <marie.garbin@frostdesigngroup.com>
2024-03-18 11:22:06 +0800

    added empty state

--- ui/src/pages/tabs/Archived.tsx
+++ ui/src/pages/tabs/Archived.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import EmptyState from '@/sections/EmptyState';
+import { InitiativesTable } from '@/components';
 
 const Archived = () => {
-  return <EmptyState />;
+  const count = 4;
+  return count > 0 ? <InitiativesTable /> : <EmptyState />;
 };
 
 export default Archived;
