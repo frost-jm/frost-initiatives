1e2736fe2776313bb225dc5cb72e2ccff9eb7719
lxpocampoFrost <117623161+lxpocampoFrost@users.noreply.github.com>
2024-03-19 15:30:48 +0800

    Feature/graph auth (#33)
    * Created typeDefs
    
    Added the base typeDefs for initiatives API,
    
    * Updated typeDefs
    
    Added missing properties for initiativeCreate
    Changed name of input initiativeStatus to initiativeTab
    
    * Updated main.tsx
    
    Added apollo client auth in main
    
    * Updated initiatives
    
    Added login for the initiatives platform
    
    * Removed unused variables and imports
    
    * Updated components-library
    
    Removed user validation function for component library
    
    * Updated main.tsx
    
    Moved apollo and auth provider to authTokenContext to fix error in client when logging in
    
    * Updated authTokenContext
    
    Added token to useEffect dependency array
    
    * Updated userContext
    
    Created state and variable to be able to retrieve name and email for currentUser
    
    * Updated userContext
    
    Instead of login button, user redirects immediately to auth0's universal login page
    
    * Added authorize and success path
    
    Added routes to prevent loginWithRedirect from going into a loop caused by callback not completing, making isAuthenticated false each render
    
    * Updated initiatives
    
    Set auth0 universal login as default page if user is not logged in
    
    * Updated context files
    
    Renamed userContext to UserContext
    Renamed authTokenContext to AuthToken
    
    * Updated app.tsx
    
    Removed logout button

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,24 +3,29 @@ import { Box } from '@mui/material';
 import LogoHeader from './sections/LogoHeader';
 import TabsLanding from './sections/TabsLanding';
 
-import { useContext } from 'react';
-import { UserContext } from './context/userContext';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Navigate } from 'react-router-dom';
 
 const App = () => {
-    const { validateUser } = useContext(UserContext);
+    const { isAuthenticated, user } = useAuth0();
 
-    return (<>
-      {validateUser(<>
-        <Box
-          sx={{
-            width: '100%',
-          }}
-        >
-          <LogoHeader />
-          <TabsLanding />
-        </Box>
-      </>)}
-    </>)
+    return (
+      <>
+        {
+          isAuthenticated && user ? 
+          <Box
+            sx={{
+              width: '100%',
+            }}
+          >
+            <LogoHeader />
+            <TabsLanding />
+          </Box> 
+          :
+          <><Navigate to="/authorize"/></>
+        }
+      </>
+    )
 };
 
 export default App;
--- ui/src/context/userContext.tsx
+++ ui/src/context/userContext.tsx
@@ -1,26 +1,32 @@
-import React, { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
+interface CurrentUserData {
+    name?: string;
+    email?: string;
+}
+
 interface UserContextData { 
-    validateUser: (component: any) => React.ReactElement | HTMLButtonElement
+    currentUser: CurrentUserData
 }
 
 export const UserContext = createContext<UserContextData>({} as UserContextData);
 
 export const UserProvider = ({ children }: any) => {
-    
-    const validateUser = (component:any) => {
-        const { isAuthenticated, loginWithRedirect } = useAuth0();
-    
-        if(isAuthenticated) {
-            return component;
-        } else {
-            return <button onClick={() => loginWithRedirect()}>Log in</button>;
+    const [currentUser, setCurrentUser] = useState({} as CurrentUserData);
+    const { isAuthenticated, user,  } = useAuth0();
+
+    if(isAuthenticated) {
+        if(user && Object.keys(currentUser).length === 0) {
+            setCurrentUser({
+                name: user?.name,
+                email: user?.email
+            })
         }
     }
 
     return (
-        <UserContext.Provider value={{ validateUser }}>
+        <UserContext.Provider value={{ currentUser }}>
           {children}
         </UserContext.Provider>
     );
