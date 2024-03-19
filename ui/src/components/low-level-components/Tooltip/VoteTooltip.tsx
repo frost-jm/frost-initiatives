79cd9b83dd0f60b6c1573a8df21daa2a93ac9fd9
Jm <jm.delasalas@frostdesigngroup.com>
2024-03-19 14:36:05 +0800

    Added tooltip on table

--- ui/src/components/low-level-components/Tooltip/VoteTooltip.tsx
+++ ui/src/components/low-level-components/Tooltip/VoteTooltip.tsx
@@ -15,12 +15,25 @@ const VoteTooltip = () => {
 				width: '100%',
 				maxWidth: '320px',
 				boxSizing: 'border-box',
+				position: 'relative',
 				'.vote-tooltip-text': {
 					marginTop: '8px',
 					color: 'rgba(29, 36, 79, 0.8)',
 					fontFamily: 'Figtree-Medium,sans-serif',
 					fontSize: '10px',
 					lineHeight: '1',
+				},
+				'&::after': {
+					content: '""',
+					position: 'absolute',
+					top: '100%',
+					left: '8px',
+					width: 22,
+					height: 11,
+					backgroundImage:
+						"url(\"data:image/svg+xml,%3Csvg width='22' height='11' viewBox='0 0 22 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.58 9.18541C12.1929 10.6987 9.80714 10.6987 8.41996 9.18541L1.04907e-06 -1.9233e-06L22 0L13.58 9.18541Z' fill='white'/%3E%3C/svg%3E%0A\")",
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
 				},
 			}}
 		>
