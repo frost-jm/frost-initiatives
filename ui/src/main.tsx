ba91d078bf58fe7fa7fe66b87371dddd326c5086
Marie <marie.garbin@frostdesigngroup.com>
2024-03-20 15:42:52 +0800

    Added voting dashboard

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,45 +4,45 @@ import App from './App.tsx';
 import './index.css';
 import ComponentsLibrary from './pages/components-library.tsx';
 import Error from './pages/error-page.tsx';
-import { UserProvider } from './context/UserContext.tsx';
+import { UserProvider } from './context/userContext.tsx';
 import { AuthTokenProvider } from './context/AuthToken.tsx';
 import Authorize from './pages/authorize.tsx';
 import Success from './pages/success.tsx';
 import React from 'react';
 
 const Index = () => {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <App />,
-			errorElement: <Error />,
-		},
-		{
-			path: '/components-library',
-			element: <ComponentsLibrary />,
-			errorElement: <Error />,
-		},
-		{
-			path: '/authorize',
-			element: <Authorize /> ,
-			errorElement: <Error />,
-		},
-		{
-			path: '/success',
-			element: <Success />,
-			errorElement: <Error />
-		}
-	]);
-	
-	return (
-	  <React.StrictMode>
-		<AuthTokenProvider>
-			<UserProvider>
-				<RouterProvider router={router} /> 
-			</UserProvider>
-		</AuthTokenProvider>
-	  </React.StrictMode>
-	);
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+    },
+    {
+      path: '/components-library',
+      element: <ComponentsLibrary />,
+      errorElement: <Error />,
+    },
+    {
+      path: '/authorize',
+      element: <Authorize />,
+      errorElement: <Error />,
+    },
+    {
+      path: '/success',
+      element: <Success />,
+      errorElement: <Error />,
+    },
+  ]);
+
+  return (
+    <React.StrictMode>
+      <AuthTokenProvider>
+        <UserProvider>
+          <RouterProvider router={router} />
+        </UserProvider>
+      </AuthTokenProvider>
+    </React.StrictMode>
+  );
 };
-  
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<Index />);
--- ui/src/pages/tabs/Voting.tsx
+++ ui/src/pages/tabs/Voting.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import EmptyState from '@/sections/EmptyState';
+import VotingBanner from '@/sections/Banners/VotingBanner';
+import Pagination from '@/components/low-level-components/Pagination';
+import { InitiativesTable, TableFilter } from '@/components';
 
 const Voting = () => {
-	return <div>Voting</div>;
+  const count = 5;
+  return count > 0 ? (
+    <>
+      <Box sx={{ padding: '12px 0 119px 0' }}>
+        <VotingBanner />
+        <Box sx={{ padding: '24px 0 16px' }}>
+          <TableFilter />
+        </Box>
+        <InitiativesTable />
+        <Box sx={{ paddingTop: '24' }}>
+          <Pagination />
+        </Box>
+      </Box>
+    </>
+  ) : (
+    <EmptyState />
+  );
 };
 
 export default Voting;
