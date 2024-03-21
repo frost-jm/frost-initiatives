ab171aa0938c9ae429e1e6c6ef9d12b0f9060efe
Marie <marie.garbin@frostdesigngroup.com>
2024-03-21 14:29:36 +0800

    Added archived page

--- ui/src/pages/tabs/Archived.tsx
+++ ui/src/pages/tabs/Archived.tsx
@@ -1,9 +1,25 @@
+import { Box } from '@mui/material';
+import { TableFilter, InitiativesTable } from '@/components';
+import Pagination from '@/components/low-level-components/Pagination';
 import EmptyState from '@/sections/EmptyState';
-import { InitiativesTable } from '@/components';
 
 const Archived = () => {
-	const count = 4;
-	return count > 0 ? <InitiativesTable /> : <EmptyState />;
+  const count = 4;
+  return count > 0 ? (
+    <>
+      <Box>
+        <Box sx={{ padding: '24px 0 16px' }}>
+          <TableFilter tableheader='Initiatives Archived' quantity={4} />
+        </Box>
+        <InitiativesTable />
+        <Box sx={{ paddingTop: '24' }}>
+          <Pagination />
+        </Box>
+      </Box>
+    </>
+  ) : (
+    <EmptyState />
+  );
 };
 
 export default Archived;
