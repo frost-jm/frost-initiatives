cf52b25ffa03cb11b63459099cce710a40fbc4e1
Jm <jm.delasalas@frostdesigngroup.com>
2024-03-26 16:36:48 +0800

    fixed batch email templates

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -3,26 +3,35 @@ require('dotenv').config();
 const postmark = require('postmark');
 const client = new postmark.Client(process.env.POSTMARK_API_KEY);
 
-async function sendEmail(data) {
-	const { commentor, author, initiativeTitle } = data;
-
-	const date = new Date();
-	const options = { month: 'long', day: 'numeric', year: 'numeric' };
-	const formattedDate = date.toLocaleString('en-US', options).replace(', ', ', ');
+async function sendEmail(comments) {
 	try {
-		return await client.sendEmailWithTemplate({
-			From: process.env.POSTMARK_SENDER,
-			To: author.email,
-			//TemplateId: process.env.POSTMARK_TEMPLATE_ID,
-			TemplateAlias: process.env.POSTMARK_TEMPLATE_ALIAS,
-			TemplateModel: {
+		const date = new Date();
+		const options = { month: 'long', day: 'numeric', year: 'numeric' };
+		const formattedDate = date.toLocaleString('en-US', options).replace(', ', ', ');
+
+		const { author, initiativeTitle } = comments[0];
+
+		const commentData = comments.map(({ commentor }) => {
+			return {
 				commenter_name: commentor.name,
 				body: commentor.comment,
 				timestamp: formattedDate,
-				notifications_url: 'where the link goes', // to be updated
-				pitch_name: initiativeTitle,
 				initials: commentor.initials,
-			},
+			};
+		});
+
+		const templateModel = {
+			comments: commentData,
+			notifications_url: 'where the link goes',
+			pitch_name: initiativeTitle,
+			action_url: '#',
+		};
+
+		await client.sendEmailWithTemplate({
+			From: process.env.POSTMARK_SENDER,
+			To: author.email,
+			TemplateAlias: process.env.POSTMARK_TEMPLATE_ALIAS,
+			TemplateModel: templateModel,
 		});
 	} catch (error) {
 		console.error(error);
